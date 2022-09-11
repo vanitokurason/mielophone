0a867662f993bb7ed166a42f097f0c6d2cff41fa
vanito-kurason <vanitocurason@gmail.com>
2022-09-11 10:59:30 +0300

    some changes in styles and forms

--- resources/views/brodskyList/bookForm.blade.php
+++ resources/views/brodskyList/bookForm.blade.php
@@ -4,24 +4,32 @@
         {{ $title }}
     </x-slot>
 
-    <form action="" method="POST">
-        @csrf
-        <label for="title">Title:</label><br>
-        <input name="title" value="{{ $post->title }}"><br>
-        <label for="author">Author:</label><br>
-        <input name="author" value="{{ $post->author }}"><br>
-        <label for="annotation">Annotation:</label><br>
-        <textarea name="annotation" cols="80" rows="17">{{ $post->bookAnnotation->annotation }}</textarea><br>
-        <label for="publication date:">Publication date:</label><br>
-        <input type="text" name="publication_date" size="80px" value="{{ $post->bookAnnotation->publication_date }}"><br>
-        <label for="Open access">Open access(1/0):</label><br>
-        <input type="text" name="open_access" size="80px" value="{{ $post->bookAnnotation->open_access }}"><br>
-        <label for="picture_path">Picture(link):</label><br>
-        <input type="text" name="picture_path" size="80px" value="{{ $post->bookAnnotation->picture_path }}"><br>
-        <label for="Alt">"Alt" for picture:</label><br>
-        <input type="text" name="alt" size="80px" value="{{ $post->bookAnnotation->alt }}"><br>
-        <input type="submit" value="Сохранить">
-        <button  name="Cancel">Отмена</button>
-    </form>
-
+    <div id="form">
+        <form action="" method="POST">
+            @csrf
+            <label for="title">Title:<br>
+                <input name="title" value="{{ $post->title }}">
+            </label><br>
+            <label for="author">Author:<br>
+                <input name="author" value="{{ $post->author }}">
+            </label><br>
+            <label for="annotation">Annotation:<br>
+                <textarea name="annotation" cols="80" rows="17">{{ $post->bookAnnotation->annotation }}</textarea>
+            </label><br>
+            <label for="publication date:">Publication date:<br>
+                <input type="text" name="publication_date" value="{{ $post->bookAnnotation->publication_date }}">
+            </label><br>
+            <label for="Open access">Open access(1/0):<br>
+                <input type="text" name="open_access" value="{{ $post->bookAnnotation->open_access }}">
+            </label><br>
+            <label for="picture_path">Picture(link):<br>
+                <input type="text" name="picture_path" value="{{ $post->bookAnnotation->picture_path }}">
+            </label><br>
+            <label for="Alt">"Alt" for picture:<br>
+                <input type="text" name="alt" value="{{ $post->bookAnnotation->alt }}">
+            </label><br>
+            <input type="submit" value="Сохранить">
+            <a href="/admin/brodsky/"><input type="button" value="Отмена/Назад"></a>
+        </form>
+    </div>
 </x-layout>
